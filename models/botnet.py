cccdb5e3dcdc523b29951877f48a331a605fbbc0
NZ99 <niccolo.zanichelli1@studenti.unipr.it>
2021-05-01 18:03:40 +0200

    Refactor BoTNet

--- models/botnet.py
+++ models/botnet.py
@@ -1,49 +1,18 @@
 from functools import partial
-from typing import Any, Tuple
+from typing import Any, Tuple, Callable, Sequence
 
 from jax import numpy as jnp
 from jax.nn import initializers
+from jax.lax import Precision
 
 from flax import linen as nn
+from flax import struct
 
 from einops import rearrange
 
-from models.botnet_config import BoTNetConfig
+from models.layers import SqueezeExciteBlock
 
 ModuleDef = Any
-
-
-class SELayer(nn.Module):
-    """Squeeze and Excite module.
-    Attributes:
-        config: BoTNet configuration
-    """
-    config: BoTNetConfig
-
-    @nn.compact
-    def __call__(self, inputs):
-        """Passes the input through a squeeze and excite block.
-        Arguments:
-            inputs:     [batch_size, height, width, dim]
-        Returns:
-            output:     [batch_size, height, width, dim]
-        """
-        cfg = self.config
-        out_dim = inputs.shape[-1]
-        se_features = max(1, int(out_dim * cfg.se_ratio))
-
-        dense = partial(nn.Dense,
-                        dtype=cfg.dtype,
-                        precision=cfg.precision,
-                        kernel_init=cfg.kernel_init,
-                        bias_init=cfg.bias_init)
-
-        y = jnp.mean(inputs, axis=(1, 2), dtype=cfg.dtype, keepdims=True)
-        y = dense(features=se_features)(y)
-        y = cfg.activation_fn(y)
-        y = dense(features=out_dim)(y)
-        y = nn.sigmoid(y) * inputs
-        return y
 
 
 class BottleneckResNetBlock(nn.Module):
@@ -55,11 +24,14 @@ class BottleneckResNetBlock(nn.Module):
         norm: a normalization module such as nn.BatchNorm
         strides: strides to use in the second convolution, either (1, 1) or (2, 2)
     """
-    config: BoTNetConfig
     filters: int
     conv: ModuleDef
     norm: ModuleDef
+    se_ratio: float = 0.0625
     strides: Tuple[int, int]
+    projection_factor: int = 4
+    activation_fn: Callable = nn.activation.swish
+    dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, inputs):
@@ -70,29 +42,30 @@ class BottleneckResNetBlock(nn.Module):
             output:     [batch_size, height, width, dim * config.projection_factor]
         """
         residual = inputs
-        cfg = self.config
 
         y = self.conv(self.filters, kernel_size=(1, 1))(inputs)
         y = self.norm()(y)
-        y = cfg.activation_fn(y)
+        y = self.activation_fn(y)
         y = self.conv(self.filters, kernel_size=(3, 3), strides=self.strides)(y)
         y = self.norm()(y)
-        y = cfg.activation_fn(y)
-        y = self.conv(self.filters * cfg.projection_factor,
+        y = self.activation_fn(y)
+        y = self.conv(self.filters * self.projection_factor,
                       kernel_size=(1, 1))(y)
         y = self.norm(scale_init=initializers.zeros)(y)
 
-        if cfg.se_ratio is not None:
-            y = SELayer(config=cfg)(y)
+        if self.se_ratio is not None:
+            y = SqueezeExciteBlock(se_ratio=self.se_ratio,
+                                   activation_fn=self.activation_fn,
+                                   dtype=self.dtype)(y)
 
         if residual.shape != y.shape:
-            residual = self.conv(self.filters * cfg.projection_factor,
+            residual = self.conv(self.filters * self.projection_factor,
                                  kernel_size=(1, 1),
                                  strides=self.strides)(residual)
             residual = self.norm()(residual)
-            residual = cfg.activation_fn(residual)
+            residual = self.activation_fn(residual)
 
-        y = cfg.activation_fn(residual + y)
+        y = self.activation_fn(residual + y)
         return y
 
 
@@ -101,7 +74,7 @@ class RelativeLogits(nn.Module):
     Attributes:
         config: BoTNet configuration
     """
-    config: BoTNetConfig
+    head_ch: int
 
     @staticmethod
     def _to_absolute_logits(rel_logits):
@@ -148,16 +121,17 @@ class RelativeLogits(nn.Module):
             output:     [batch_size, heads, height, width, height, width]
         """
 
-        cfg = self.config
         _, _, H, W, _ = query.shape
 
-        rel_pos_emb_w_shape = (2 * W - 1, cfg.head_dim)
-        rel_pos_emb_w = self.param('rel_pos_emb_w', cfg.posemb_init,
-                                   rel_pos_emb_w_shape)
+        rel_pos_emb_w_shape = (2 * W - 1, self.head_ch)
+        rel_pos_emb_w = self.param(
+            'rel_pos_emb_w', initializers.normal(stddev=self.head_ch**-0.5),
+            rel_pos_emb_w_shape)
 
-        rel_pos_emb_h_shape = (2 * H - 1, cfg.head_dim)
-        rel_pos_emb_h = self.param('rel_pos_emb_h', cfg.posemb_init,
-                                   rel_pos_emb_h_shape)
+        rel_pos_emb_h_shape = (2 * H - 1, self.head_ch)
+        rel_pos_emb_h = self.param(
+            'rel_pos_emb_h', initializers.normal(stddev=self.head_ch**-0.5),
+            rel_pos_emb_h_shape)
 
         rel_logits_w = self._relative_logits_1d(query, rel_pos_emb_w)
         rel_logits_w = rearrange(rel_logits_w, 'b h H I W V -> b h H W I V')
@@ -174,49 +148,55 @@ class BoTMHSA(nn.Module):
     Attributes:
         config: BoTNet configuration
     """
-    config: BoTNetConfig
+    num_heads: int
+    head_ch: int
+
+    dtype: jnp.dtype = jnp.float32
+    precision: Precision = Precision.DEFAULT
+    kernel_init: Callable = initializers.he_uniform()
+    bias_init: Callable = initializers.normal(stddev=1e-6)
 
     @nn.compact
-    def __call__(self, inputs_q):
+    def __call__(self, inputs):
         """Applies multi-head self-attention on the input data.
         Arguments:
             inputs_q:   [batch_size, height, width, dim]
         Returns:
             output:     [batch_size, height, width, dim]
         """
-        cfg = self.config
         conv = partial(nn.Conv,
-                       features=cfg.num_heads * cfg.head_dim,
+                       features=self.num_heads * self.head_ch,
                        kernel_size=(1, 1),
                        use_bias=False,
-                       precision=cfg.precision,
-                       kernel_init=cfg.kernel_init)
+                       precision=self.precision,
+                       kernel_init=self.kernel_init)
 
-        query, key, value = (conv(dtype=cfg.dtype, name="query")(inputs_q),
-                             conv(dtype=cfg.dtype, name="key")(inputs_q),
-                             conv(dtype=cfg.dtype, name="value")(inputs_q))
+        query, key, value = (conv(dtype=self.dtype, name="query")(inputs),
+                             conv(dtype=self.dtype, name="key")(inputs),
+                             conv(dtype=self.dtype, name="value")(inputs))
         query, key, value = (rearrange(query,
                                        'b H W (h d) -> b h H W d',
-                                       h=cfg.num_heads),
+                                       h=self.num_heads),
                              rearrange(key,
                                        'b H W (h d) -> b h H W d',
-                                       h=cfg.num_heads),
+                                       h=self.num_heads),
                              rearrange(value,
                                        'b H W (h d) -> b h H W d',
-                                       h=cfg.num_heads))
+                                       h=self.num_heads))
 
-        query = query / jnp.sqrt(cfg.head_dim).astype(cfg.dtype)
+        query = query / jnp.sqrt(self.head_dim).astype(self.dtype)
 
         attn_weights = jnp.einsum('b h H W d, b h P Q d -> b h H W P Q',
                                   query,
                                   key,
-                                  precision=cfg.precision)
-        attn_weights = attn_weights + RelativeLogits(config=cfg)(query)
-        attn_weights = nn.softmax(attn_weights).astype(cfg.dtype)
+                                  precision=self.precision)
+        attn_weights = attn_weights + RelativeLogits(
+            head_ch=self.head_ch)(query)
+        attn_weights = nn.softmax(attn_weights).astype(self.dtype)
         attn_out = jnp.einsum('b h H W P Q, b h H W d -> b H W h d',
                               attn_weights,
                               value,
-                              precision=cfg.precision)
+                              precision=self.precision)
         attn_out = rearrange(attn_out, 'b H W h d -> b H W (h d)')
         return attn_out
 
@@ -230,11 +210,12 @@ class BoTBlock(nn.Module):
         norm: a normalization module such as nn.BatchNorm
         strides: strides to use in the second convolution, either (1, 1) or (2, 2)
     """
-    config: BoTNetConfig
     filters: int
     conv: ModuleDef
     norm: ModuleDef
+    projection_factor: int = 4
     strides: Tuple[int, int]
+    activation_fn: ModuleDef = nn.swish
 
     @nn.compact
     def __call__(self, inputs):
@@ -249,7 +230,7 @@ class BoTBlock(nn.Module):
 
         y = self.conv(self.filters, kernel_size=(1, 1))(inputs)
         y = self.norm()(y)
-        y = cfg.activation_fn(y)
+        y = self.activation_fn(y)
         y = BoTMHSA(config=cfg)(y)
         if self.strides == (2, 2):
             y = nn.avg_pool(y,
@@ -257,19 +238,19 @@ class BoTBlock(nn.Module):
                             strides=self.strides,
                             padding='SAME')
         y = self.norm()(y)
-        y = cfg.activation_fn(y)
-        y = self.conv(self.filters * cfg.projection_factor,
+        y = self.activation_fn(y)
+        y = self.conv(self.filters * self.projection_factor,
                       kernel_size=(1, 1))(y)
         y = self.norm(scale_init=initializers.zeros)(y)
 
         if self.strides == (2, 2) or residual.shape != y.shape:
-            residual = self.conv(self.filters * cfg.projection_factor,
+            residual = self.conv(self.filters * self.projection_factor,
                                  kernel_size=(1, 1),
                                  strides=self.strides)(residual)
             residual = self.norm()(residual)
-            residual = cfg.activation_fn(residual)
+            residual = self.activation_fn(residual)
 
-        y = cfg.activation_fn(residual + y)
+        y = self.activation_fn(residual + y)
         return y
 
 
@@ -278,7 +259,24 @@ class BoTNet(nn.Module):
     Attributes:
         config: BoTNet configuration
     """
-    config: BoTNetConfig
+
+    stage_sizes: Sequence[int] = struct.field(
+        default_factory=lambda: [3, 4, 6, 6], pytree_node=False)
+    num_classes: int = 1000
+    stride_one: bool = True
+    se_ratio: float = 0.0625
+    activation_fn: ModuleDef = nn.swish
+    num_heads: int = 4
+    head_dim: int = 128
+    initial_filters: int = 64
+    projection_factor: int = 4
+    bn_momentum: float = 0.9
+    bn_epsilon: float = 1e-5
+    dtype: jnp.dtype = jnp.float32
+    precision: Any = Precision.DEFAULT
+    kernel_init: Callable = initializers.he_uniform()
+    bias_init: Callable = initializers.normal(stddev=1e-6)
+    posemb_init: Callable = initializers.normal(stddev=head_dim**-0.5)
 
     @nn.compact
     def __call__(self, inputs, train: bool = True):
@@ -289,44 +287,49 @@ class BoTNet(nn.Module):
         Returns:
             output:     [batch_size, config.num_classes]
         """
-        cfg = self.config
         conv = partial(nn.Conv,
                        use_bias=False,
-                       dtype=cfg.dtype,
-                       precision=cfg.precision,
-                       kernel_init=cfg.kernel_init)
+                       dtype=self.dtype,
+                       precision=self.precision,
+                       kernel_init=self.kernel_init)
         norm = partial(nn.BatchNorm,
                        use_running_average=not train,
-                       momentum=cfg.bn_momentum,
-                       epsilon=cfg.bn_epsilon,
-                       dtype=cfg.dtype)
+                       momentum=self.bn_momentum,
+                       epsilon=self.bn_epsilon,
+                       dtype=self.dtype)
 
-        y = conv(cfg.initial_filters,
+        y = conv(self.initial_filters,
                  kernel_size=(7, 7),
                  strides=(2, 2),
                  padding=[(3, 3), (3, 3)])(inputs)
         y = norm()(y)
-        y = cfg.activation_fn(y)
+        y = self.activation_fn(y)
         y = nn.max_pool(y, (3, 3), strides=(2, 2), padding='SAME')
-        for i, block_size in enumerate(cfg.stage_sizes[:-1]):
+        for i, block_size in enumerate(self.stage_sizes[:-1]):
             for j in range(block_size):
                 strides = (2, 2) if i > 0 and j == 0 else (1, 1)
-                y = BottleneckResNetBlock(filters=cfg.initial_filters * 2**i,
-                                          strides=strides,
-                                          config=cfg,
-                                          conv=conv,
-                                          norm=norm)(y)
-        for j in range(cfg.stage_sizes[-1]):
-            strides = (2, 2) if j == 0 and cfg.stride_one is False else (1, 1)
-            y = BoTBlock(filters=cfg.initial_filters * 2**(i + 1),
+                y = BottleneckResNetBlock(
+                    filters=self.initial_filters * 2**i,
+                    strides=strides,
+                    conv=conv,
+                    norm=norm,
+                    se_ratio=self.se_ratio,
+                    projection_factor=self.projection_factor,
+                    activation_fn=self.activation_fn,
+                    dtype=self.dtype,
+                )(y)
+        for j in range(self.stage_sizes[-1]):
+            strides = (2, 2) if j == 0 and self.stride_one is False else (1, 1)
+            y = BoTBlock(filters=self.initial_filters * 2**(i + 1),
                          strides=strides,
-                         config=cfg,
                          conv=conv,
-                         norm=norm)(y)
+                         norm=norm,
+                         projection_factor=self.projection_factor,
+                         activation_fn=self.activation_fn)(y)
         y = jnp.mean(y, axis=(1, 2))
-        y = nn.Dense(cfg.num_classes,
-                     dtype=cfg.dtype,
-                     kernel_init=cfg.kernel_init,
-                     bias_init=cfg.bias_init)(y)
-        y = jnp.asarray(y, dtype=cfg.dtype)
+        y = nn.Dense(self.num_classes,
+                     dtype=self.dtype,
+                     kernel_init=self.kernel_init,
+                     bias_init=self.bias_init)(y)
+        y = jnp.asarray(y, dtype=self.dtype)
         return y
