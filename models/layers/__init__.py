2dfb550d4a4bc5fdd75add109fc902d07206cdfa
Martino Mansoldo <mansoldm@tcd.ie>
2021-05-05 13:46:43 +0100

    add rope

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -1,5 +1,5 @@
 from models.layers.feedforwards import FFBlock, LeFFBlock
 from models.layers.stems import Image2TokenBlock, PatchEmbedBlock
 from models.layers.squeeze_excite import SqueezeExciteBlock
-from models.layers.position_embed import AddAbsPosEmbed
+from models.layers.position_embed import AddAbsPosEmbed, RotaryPositionalEmbedding, FixedPositionalEmbedding
 from models.layers.attentions import SelfAttentionBlock, CvTSelfAttentionBlock
--- models/layers/position_embed.py
+++ models/layers/position_embed.py
@@ -1,7 +1,48 @@
 from typing import Callable
 
+import jax.numpy as jnp
+from einops import rearrange, repeat
 from flax.linen import initializers
 from flax import linen as nn
+
+
+def rotate_every_two(x):
+    x1 = x[:, :, ::2]
+    x2 = x[:, :, 1::2]
+
+    x = jnp.stack((-x2, x1), axis=-1)
+
+    return rearrange(x, '... d j -> ... (d j)')
+
+
+def apply_rotary_pos_emb(x, sincos):
+    sin, cos = map(lambda t: repeat(t, 'b n -> b (n j)', j=2)[:, None, :], sincos)
+    return (x * cos) + (rotate_every_two(x) * sin)
+
+
+class FixedPositionalEmbedding(nn.Module):
+
+    @nn.compact
+    def __call__(self, inputs, seq_dim=0):
+        dim = inputs.shape[-1]
+        intervals = jnp.arange(start=0, stop=self.dim, step=2, dtype=self.dtype)
+        inv_freq = 1. / (10e4 ** intervals / dim)
+        t = jnp.arange(inputs.shape[seq_dim], dtype=self.dtype)
+
+        freqs = jnp.einsum('i , j -> i j', t, inv_freq)
+        emb = jnp.concatenate([freqs, freqs], axis=-1)
+
+        return emb
+
+
+class RotaryPositionalEmbedding(FixedPositionalEmbedding):
+    dtype: jnp.dtype = jnp.float32
+
+    @nn.compact
+    def __call__(self, inputs, seq_dim=0):
+        sincos = super(RotaryPositionalEmbedding, self).__call__(inputs, seq_dim)
+        emb = apply_rotary_pos_emb(inputs, sincos)
+        return emb
 
 
 class AddAbsPosEmbed(nn.Module):
