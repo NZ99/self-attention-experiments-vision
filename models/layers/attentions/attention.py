e90fe69e2aa6f674fa08b577e0fd05d1ca24ebe1
NZ99 <niccolo.zanichelli1@studenti.unipr.it>
2021-05-07 00:42:04 +0200

    Fix SelfAttentionBlock to use inheritance

--- models/layers/attentions/attention.py
+++ models/layers/attentions/attention.py
@@ -89,31 +89,8 @@ class AttentionBlock(nn.Module):
         return output
 
 
-class SelfAttentionBlock(nn.Module):
-    num_heads: int
-    head_ch: Optional[int] = None
-    out_ch: Optional[int] = None
-    talking_heads: bool = False
-    attn_drop_rate: float = 0.
-    out_drop_rate: float = 0.
-    use_bias: bool = False
-    dtype: jnp.dtype = jnp.float32
-    precision: Precision = Precision.DEFAULT
-    kernel_init: Callable = initializers.kaiming_uniform()
-    bias_init: Callable = initializers.zeros
+class SelfAttentionBlock(AttentionBlock):
 
     @nn.compact
     def __call__(self, inputs, is_training: bool):
-        return AttentionBlock(num_heads=self.num_heads,
-                              head_ch=self.head_ch,
-                              out_ch=self.out_ch,
-                              talking_heads=self.talking_heads,
-                              attn_drop_rate=self.attn_drop_rate,
-                              out_drop_rate=self.out_drop_rate,
-                              use_bias=self.use_bias,
-                              dtype=self.dtype,
-                              precision=self.precision,
-                              kernel_init=self.kernel_init,
-                              bias_init=self.bias_init)(inputs,
-                                                        inputs,
-                                                        is_training=is_training)
+        return super().__call__(inputs, inputs, is_training=is_training)
