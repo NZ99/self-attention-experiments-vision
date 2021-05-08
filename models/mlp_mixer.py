98458993006358e8499dd69dd97970efea4267d0
NZ99 <niccolo.zanichelli1@studenti.unipr.it>
2021-05-08 16:02:49 +0200

    Fix MLPMixer

--- models/mlp_mixer.py
+++ models/mlp_mixer.py
@@ -10,23 +10,23 @@ from models.layers import PatchEmbedBlock, FFBlock
 class MixerBlock(nn.Module):
     tokens_expand_ratio: float
     channels_expand_ratio: float
-    activation_fn = nn.activation.gelu
+    activation_fn: Callable = nn.activation.gelu
     dtype: jnp.dtype = jnp.float32
 
     @nn.compact
-    def __call__(self, inputs):
-        x = nn.LayerNorm(dtype=self.dype)(inputs)
+    def __call__(self, inputs, is_training: bool):
+        x = nn.LayerNorm(dtype=self.dtype)(inputs)
         x = rearrange(x, '... l d -> ... d l')
         x = FFBlock(expand_ratio=self.tokens_expand_ratio,
                     activation_fn=self.activation_fn,
-                    dype=self.dtype)(x)
+                    dtype=self.dtype)(x, is_training=is_training)
         x = rearrange(x, '... d l -> ... l d')
         x = x + inputs
 
         y = nn.LayerNorm(dtype=self.dtype)(x)
         y = FFBlock(expand_ratio=self.channels_expand_ratio,
                     activation_fn=self.activation_fn,
-                    dtype=self.dtype)(y)
+                    dtype=self.dtype)(y, is_training=is_training)
         output = x + y
         return output
 
@@ -35,24 +35,23 @@ class MLPMixer(nn.Module):
     num_classes: int
     num_layers: int
     embed_dim: int
-    patch_shape: Tuple[int, int]
+    patch_shape: Tuple[int]
     tokens_expand_ratio: float = 0.5
     channels_expand_ratio: float = 4
     activation_fn: Callable = nn.activation.gelu
     dtype: jnp.dtype = jnp.float32
 
     @nn.compact
-    def __call__(self, inputs, *unused_args, **unused_kwargs):
+    def __call__(self, inputs, is_training: bool):
         x = PatchEmbedBlock(patch_shape=self.patch_shape,
                             embed_dim=self.embed_dim,
                             dtype=self.dtype)(inputs)
-        x = rearrange(x, 'b h w d -> b (h w) d')
 
         for _ in range(self.num_layers):
             x = MixerBlock(tokens_expand_ratio=self.tokens_expand_ratio,
                            channels_expand_ratio=self.channels_expand_ratio,
                            activation_fn=self.activation_fn,
-                           dtype=self.dtype)(x)
+                           dtype=self.dtype)(x, is_training=is_training)
 
         x = nn.LayerNorm(dtype=self.dtype)(x)
         x = jnp.mean(x, axis=1)
